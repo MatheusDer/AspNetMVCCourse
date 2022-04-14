d21b3b22827e265d49e985217943d50ca3d1f6c4
MatheusDer <matheusder1@hotmail.com>
2022-04-14 12:39:56 -0300

    Add project files.

---
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,104 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _db.Categories;
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]       
+        public IActionResult Create(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfuly";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound(id);
+
+            // var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            var category = _db.Categories.Find(id);
+            if (category == null)
+                return NotFound(category);
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited successfuly";
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound(id);
+
+            var category = _db.Categories.Find(id);
+            if (category == null)
+                return NotFound(category);
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            var category = _db.Categories.Find(id);
+
+            if (category == null)
+                return NotFound(id);
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfuly";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
