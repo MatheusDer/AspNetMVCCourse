21a963ed191012ceb93419d657518320bb8da8eb
MatheusDer <matheusder1@hotmail.com>
2022-04-14 13:45:50 -0300

    (abby): add db connection

---
+++ Abby/AbbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AbbyWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbbyWeb.Data
+{
+    public class ApplicationDbContext : DbContext  //add-migration <migrationName> database-update
+    {
+        public DbSet<Category> Categories { get; set; } //Name of the table in database
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+    }
+}
