8e6dbd1abeb8dec676f2d39821bb7d126479e1a3
MatheusDer <matheusder1@hotmail.com>
2022-04-14 13:00:25 -0300

    (abby): create category model

---
+++ Abby/AbbyWeb/Model/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }  //Is required by default, as int is not a nullable
+    }
+}
