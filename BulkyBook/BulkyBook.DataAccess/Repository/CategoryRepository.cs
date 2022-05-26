694a70606f2d49772b409167ef7a33e9eb445654
Matheus Der <matheusder1@hotmail.com>
2022-05-26 15:40:46 -0300

    feat(data): implement category repository class

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context) 
+            : base(context)
+        { 
+            _context = context;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(Category cateagory)
+        {
+            _context.Update(cateagory);
+        }
+    }
+}
