b8347c010db9d60c803b9c2e059abf7f3a43c270
Matheus Der <matheusder1@hotmail.com>
2022-06-01 14:36:05 -0300

    feat(web): using viewdata and viewbag to create a list of items for the user to choose

---
+++ BulkyBook/BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,103 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _unitOfWork.Category.GetAll();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+                u => new SelectListItem 
+                { 
+                    Value = u.Id.ToString(), Text = u.Name 
+                });
+
+            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Value = u.Id.ToString(),
+                    Text = u.Name
+                });
+
+            if (id == null || id == 0)
+            {
+                ViewBag.CategoryList = CategoryList;
+                ViewData["CoverTypeList"] = CoverTypeList;
+                return View(product);
+            }
+            else
+            {
+
+            }
+
+            
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(category);
+                _unitOfWork.Save();
+                TempData["success"] = "Category edited successfuly";
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound(id);
+
+            var category = _unitOfWork.Category.GetFirstOrDefault(c => c.Id == id);
+            if (category == null)
+                return NotFound(category);
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            var category = _unitOfWork.Category.GetFirstOrDefault(c => c.Id == id);
+
+            if (category == null)
+                return NotFound(id);
+
+            _unitOfWork.Category.Remove(category);
+            _unitOfWork.Save();
+            TempData["success"] = "Category deleted successfuly";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
