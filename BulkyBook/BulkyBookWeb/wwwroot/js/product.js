26008633da646ae4183387e17b892fe45f935375
Matheus Der <matheusder1@hotmail.com>
2022-06-14 15:28:10 -0300

    feat(web): loading data tables

---
+++ BulkyBook/BulkyBookWeb/wwwroot/js/product.js
@@ -0,0 +1,15 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    $('#myTable').DataTable({
+        "ajax": {
+            "url": "/Admin/Product/GetAll"
+        },
+        "columns": [
+            { "data": "title", "width": "15%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "price", "width": "15%" },
+            { "data": "author", "width": "15%" },
+        ]
+    });
+});
