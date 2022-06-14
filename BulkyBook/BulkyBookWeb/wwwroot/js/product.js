e7f4adf8ac0b71d7372c41c8bc70115b814f39b8
Matheus Der <matheusder1@hotmail.com>
2022-06-14 15:53:51 -0300

    feat(web): render edit and delete button in the data tables

--- BulkyBook/BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBook/BulkyBookWeb/wwwroot/js/product.js
@@ -6,11 +6,22 @@ $(document).ready(function () {
             "url": "/Admin/Product/GetAll"
         },
         "columns": [
-            { "data": "title", "width": "15%" },
-            { "data": "isbn", "width": "15%" },
-            { "data": "price", "width": "15%" },
-            { "data": "author", "width": "15%" },
-            { "data": "category.name", "width": "15%" },
+            { "data": "title"},
+            { "data": "isbn"},
+            { "data": "price"},
+            { "data": "author"},
+            { "data": "category.name"},
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-100 btn-group" role="group">
+							<a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-2 rounded w-100"><i class="bi bi-pencil-square"></i> Edit</a>
+							<a href="/Admin/Product/Delete?id=${data}" class="btn btn-danger mx-2 rounded w-100"><i class="bi bi-file-earmark-x"></i> Delete</a>
+						</div>
+                        `
+                }
+            },
         ]
     });
 });
