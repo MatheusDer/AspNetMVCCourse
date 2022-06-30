071d1e208a43816af2fa79110b7ca0a0c44acd89
Matheus Der <matheusder1@hotmail.com>
2022-06-30 16:10:44 -0300

    feat(model | data): add more props to identity user

--- BulkyBook/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -9,6 +9,7 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<CoverType> CoverTypes { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) 
