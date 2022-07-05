2bb2eff19ffb89a2e210519207b61797f781382a
Matheus Der <matheusder1@hotmail.com>
2022-07-05 13:21:01 -0300

    feat(web | model): add price calculation based on quantity

--- BulkyBook/BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBook/BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private ShoppingCartVM ShoppingCart;
+
+        public int OrderTotal { get; set; } 
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        //[AllowAnonymous]
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ShoppingCart = new ShoppingCartVM()
+            {
+                Carts = _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value, includeProperties: "Product")
+            };
+
+            foreach(var cart in ShoppingCart.Carts)
+                cart.Price = GetPrice(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+
+            return View(ShoppingCart);
+        }
+
+        private double GetPrice(double quantity, double price, double price50, double price100)
+        {
+            if (quantity <= 50)
+                return price;
+
+            if (quantity <= 100)
+                return price50;
+
+            return price100;
         }
     }
 }
