d74dd0c90a672c87a1a48cd7617dd03838d8bc27
Matheus Der <matheusder1@hotmail.com>
2022-07-06 15:27:39 -0300

    feat(web): add post method for summary

--- BulkyBook/BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBook/BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,5 +1,7 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -70,6 +72,49 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             }
 
             return View(ShoppingCart);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Summary(ShoppingCartVM shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            shoppingCart.Carts = _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value, includeProperties: "Product");
+
+            shoppingCart.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
+            shoppingCart.OrderHeader.OrderStatus = SD.OrderStatusPending;
+            shoppingCart.OrderHeader.OrderDate = DateTime.Now;
+            shoppingCart.OrderHeader.ApplicationUserId = claim.Value;
+
+            foreach (var cart in shoppingCart.Carts)
+            {
+                cart.Price = GetPrice(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+
+                shoppingCart.OrderHeader.OrderTotal += cart.Price * cart.Count;
+            }
+
+            _unitOfWork.OrderHeader.Add(shoppingCart.OrderHeader);
+            _unitOfWork.Save();
+
+            foreach (var cart in shoppingCart.Carts)
+            {
+                OrderDetail orderDetail = new()
+                {
+                    ProductId = cart.ProductId,
+                    OrderHeaderId = shoppingCart.OrderHeader.Id,
+                    Price = cart.Price,
+                    Count = cart.Count
+                };
+                _unitOfWork.OrderDetail.Add(orderDetail);
+                _unitOfWork.Save();
+            }
+
+            _unitOfWork.ShoppingCart.Remove(shoppingCart.Carts);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Plus(int cartId)
