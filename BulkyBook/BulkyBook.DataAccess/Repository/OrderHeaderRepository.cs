a86ec6710174bb5de52288d7e93e41006f623872
Matheus Der <matheusder1@hotmail.com>
2022-07-06 11:12:03 -0300

    feat(data): create unit of work and repository for order header and detail

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,38 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderHeaderRepository(ApplicationDbContext context) 
+            : base(context)
+        {
+            _context = context;
+        }
+
+        public void Update(OrderHeader orderHeader)
+        {
+            _context.OrderHeaders.Update(orderHeader);
+        }
+
+        public void UpdateStatus(int id, string status, string? paymentStats = null)
+        {
+            var orderHeader = _context.OrderHeaders.FirstOrDefault(x => x.Id == id);
+
+            if (orderHeader is not null)
+            {
+                orderHeader.OrderStatus = status;
+                if (paymentStats is not null)
+                    orderHeader.PaymentStatus = paymentStats;
+            }
+        }
+    }
+}
