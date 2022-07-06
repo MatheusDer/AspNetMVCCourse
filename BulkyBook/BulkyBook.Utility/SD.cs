9ac581f57d502f496fc9b5eecc7093ab05bc6deb
Matheus Der <matheusder1@hotmail.com>
2022-07-06 15:27:06 -0300

    feat(util): create region for order and payment status

--- BulkyBook/BulkyBook.Utility/SD.cs
+++ BulkyBook/BulkyBook.Utility/SD.cs
@@ -8,9 +8,27 @@ namespace BulkyBook.Utility
 {
     public static class SD
     {
+        #region ROLES
         public const string Role_User_Indi = "Individual";
         public const string Role_User_Comp = "Company";
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
+        #endregion
+
+        #region OrderStatus
+        public const string OrderStatusPending = "Pending";
+        public const string OrderStatusApproved = "Approved";
+        public const string OrderStatusInProcess = "Process";
+        public const string OrderStatusShipped = "Shipped";
+        public const string OrderStatusCanceled = "Canceled";
+        public const string OrderStatusRefunded = "Refunded";
+        #endregion
+
+        #region PaymentStatus
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
+        #endregion
     }
 }
