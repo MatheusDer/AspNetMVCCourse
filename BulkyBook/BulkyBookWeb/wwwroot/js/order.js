7328cbec991ca0efcc5e6d80487e580c0f1a2a18
Matheus Der <matheusder1@hotmail.com>
2022-07-08 16:05:32 -0300

    featt(web): populate order index view

--- BulkyBook/BulkyBookWeb/wwwroot/js/order.js
+++ BulkyBook/BulkyBookWeb/wwwroot/js/order.js
@@ -21,7 +21,7 @@ function loadDataTable() {
                 "render": function (data) {
                     return `
                         <div class="w-100 btn-group" role="group">
-							<a href="/Admin/Order/Details?orderId=${data}" class="btn btn-primary mx-2 rounded w-100"><i class="bi bi-pencil-square"></i> Details</a>
+							<a href="/Admin/Order/Details?orderId=${data}" class="btn btn-primary mx-2 rounded w-100"><i class="bi bi-folder-symlink"></i></a>
 						</div>
                         `
                 }
