611efdd8d8ef04cc0171788912c99b403d22f817
Matheus Der <matheusder1@hotmail.com>
2022-07-08 15:53:39 -0300

    feat(web): create order view table

---
+++ BulkyBook/BulkyBookWeb/wwwroot/js/order.js
@@ -0,0 +1,31 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $('#myTable').DataTable({
+        "ajax": {
+            "url": "/Admin/Order/GetAll"
+        },
+        "columns": [
+            { "data": "id" },
+            { "data": "name" },
+            { "data": "phoneNumber" },
+            { "data": "applicationUser.email" },
+            { "data": "orderStatus" },
+            { "data": "orderTotal" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-100 btn-group" role="group">
+							<a href="/Admin/Order/Details?orderId=${data}" class="btn btn-primary mx-2 rounded w-100"><i class="bi bi-pencil-square"></i> Details</a>
+						</div>
+                        `
+                }
+            },
+        ]
+    });
+};
