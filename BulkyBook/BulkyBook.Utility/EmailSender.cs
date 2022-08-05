c9b91f473a76c6d70f82c60c6768e5186096013b
Matheus Der <matheusder1@hotmail.com>
2022-08-05 18:39:16 -0300

    fix(util): implement the email sender

--- BulkyBook/BulkyBook.Utility/EmailSender.cs
+++ BulkyBook/BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +12,32 @@ namespace BulkyBook.Utility
 {
     public class EmailSender : IEmailSender
     {
+        private readonly IConfiguration _configuration;
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
-            => Task.CompletedTask;
+        {
+            var adminEmail = _configuration["adminEmail"];
+            var emailPassword = _configuration["emailPassword"];
+
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse(adminEmail));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+            //send email
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                emailClient.Authenticate(adminEmail, emailPassword);
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+            }
+
+            return Task.CompletedTask;
+        }
     }
 }
